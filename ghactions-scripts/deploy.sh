af8f6330966aaf0bf80c1e2c9c7d177f6d685e40
matias palmero <matias.palmero@cloudhesive.com>
2023-07-31 19:14:46 -0300

    testing script

--- ghactions-scripts/deploy.sh
+++ ghactions-scripts/deploy.sh
@@ -1,39 +1,3 @@
-#/bin/bash
-
-stack_name="$1"
-template_url="$2"
-
-# Use the environment variables directly
-echo "Received parameter: InstanceType=${InstanceType}"
-echo "Received parameter: Monitoring=${Monitoring}"
-
-# Remove the first two positional parameters (stack_name and template_url)
-shift 2
-
-# Process dynamic parameters as key-value pairs
-while [[ $# -gt 0 ]]; do
-  key="$1"
-  value="$2"
-
-  # Validate key and value format
-  if [[ "$key" == ParameterKey=* && "$value" == ParameterValue=* ]]; then
-    # Extract the parameter name and value without the prefixes
-    parameter_key="${key#ParameterKey=}"
-    parameter_value="${value#ParameterValue=}"
-    
-    echo "Received parameter: $parameter_key=$parameter_value"
-    # Use the parameter as needed in your script logic...
-  else
-    echo "Error: Invalid parameter format: $key $value"
-    exit 1
-  fi
-
-  # Shift the processed key-value pair
-  shift 2
-done
-
-# Your existing script logic goes here...
-
 if ! update_output=$(aws cloudformation update-stack --stack-name "$stack_name" --template-url "$template_url" --parameters "$parameters" 2>&1); then
   if [[ $update_output == *"No updates are to be performed."* ]]; then
     echo "No updates needed for the stack."
